4ee97537b49f27adf9ad1df0aad7efee7ea7a2ac
JD <155418256+JHLEE1000@users.noreply.github.com>
2024-05-24 01:16:03 +0900

    [Bronze V] Title: 시험 성적, Time: 204 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -8,8 +8,13 @@ public class Main {
         
         sc.close();
         
-        //삼항연산자 사용
-        System.out.println((s>=90)?"A":(s>=80)?"B":(s>=70)?"C":(s>=60)?"D":"F");
+        if (s >= 0 && s <= 100) {
+            //삼항연산자 사용
+            System.out.println((s>=90)?"A":(s>=80)?"B":(s>=70)?"C":(s>=60)?"D":"F");            
+        } else {
+            System.out.println("획득 가능한 시험 점수의 범위값을 벗어났습니다.");
+        }
+
         
 /**        if ( s >= 90 && s <= 100 ) {
             System.out.println("A");
