93985e30cf6c016fb24337f307fb4dd194137ff3
JD <155418256+JHLEE1000@users.noreply.github.com>
2024-05-30 01:06:43 +0900

    [Bronze V] Title: 윤년, Time: 208 ms, Memory: 17764 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -7,11 +7,20 @@ public class Main {
         int y;
         
         y = sc.nextInt();
+        sc.close();
+
+        if(y%4==0) {
+            if(y%400==0) System.out.print(1);
+            else if(y%100==0) System.out.print(0);
+            else System.out.print(1);
+        } 
+        else System.out.print(0);
         
-        if ( y % 4 == 0 && (y % 100 != 0 || y % 400 == 0) ) {
-            System.out.print( 1 );
-        } else {
-            System.out.print( 0 );
-        }
+//        if ( y % 4 == 0 && (y % 100 != 0 || y % 400 == 0) ) { 
+//            System.out.print( 1 ); 
+//        } else {
+//            System.out.print( 0 );
+//        }
+    
     }
 }
