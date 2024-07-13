5a640b117b7cb2f6fbe583504975e8f18373a198
JD <155418256+JHLEE1000@users.noreply.github.com>
2024-07-14 01:59:58 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int price) {
+        double discountRate = 1.0;
+        
+        if (price >= 500000) {
+            discountRate = 0.8;
+        } else if (price >= 300000) {
+            discountRate = 0.9;
+        } else if (price >= 100000) {
+            discountRate = 0.95;
+        }
+        
+        return (int) (price * discountRate);
+    }
+}
