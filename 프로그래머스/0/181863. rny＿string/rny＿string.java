697366460707f48e90779c440433a41910d1f789
JD <155418256+JHLEE1000@users.noreply.github.com>
2024-07-31 11:05:53 +0900

    [level 0] Title: rny_string, Time: 0.07 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String rny_string) {
+        
+        StringBuilder answer = new StringBuilder();
+        
+        for (char c : rny_string.toCharArray()) {
+            if(c == 'm') {
+                answer.append("rn");
+            } else {
+                answer.append(c);
+            }
+        }
+        
+        return answer.toString();
+    }
+}
